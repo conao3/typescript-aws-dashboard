af5357cd75bb6cf9d3d7847c86ad0674b82c7304
conao3 <conao3@gmail.com>
2026-02-08 16:48:48 +0900

    first commit

---
+++ flake.nix
@@ -0,0 +1,67 @@
+{
+  description = "rust and typescript development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
+
+      perSystem =
+        {
+          pkgs,
+          system,
+          config,
+          ...
+        }:
+        let
+          overlay =
+            final: prev:
+            let
+              nodejs = prev.nodejs_22;
+              pnpm = prev.pnpm_10.override { inherit nodejs; };
+            in
+            {
+              inherit nodejs pnpm;
+            };
+          pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [ overlay ];
+          };
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              rustc
+              cargo
+              rust-analyzer
+              rustfmt
+              clippy
+              nodejs
+              pnpm
+            ];
+          };
+
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs = {
+              nixfmt.enable = true;
+              rustfmt.enable = true;
+              prettier.enable = true;
+            };
+          };
+        };
+    };
+}
