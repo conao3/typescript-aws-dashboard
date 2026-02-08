f87624239befdd2a1bd6907a2aad0d9a0030bfeb
conao3 <conao3@gmail.com>
2026-02-08 17:58:58 +0900

    fmt

--- backend/src/db.rs
+++ backend/src/db.rs
@@ -1,5 +1,5 @@
-use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 
 pub async fn create_pool() -> Result<PgPool, sqlx::Error> {
     let database_url = std::env::var("DATABASE_URL")
